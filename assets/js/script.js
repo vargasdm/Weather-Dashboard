b915dcaa914f5f6a00da161da727aed8e52d3d62
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-11-16 18:14:14 -0500

    Initial Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+var APIKey = "02033f41b7ba4948cda8476745ac5025";
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+    // need text area where I can input a city
+    // need a click event listener for search button
+        // need a function that will use the textarea input as parameters ( somhow convert the name into coordinates that can be used by the api ) for the fetch function
+            
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+
+            // need fetch function to grab current city name, date, icon that represents weather conditions (Im not sure if that is somehting included in the api or something I have to make), temp, humidity, and windspeed all based on teh input from the text area
+                // this info will need to be rendered to speciofic div 
+            
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+                // need another fetch function to grabs the 5-day forecast date, icon that represents weather conditions (Im not sure if that is somehting included in the api or something I have to make), temp, humidity, and windspeed all based on teh input from the text area
+                // not sure if it will all go in one div or 5 different divs for each date
+        // need function that will save the data that is fetched in other functions (current and 5 day forecast) to the local storage as an object or an object array????????? (with each  of teh fetch parameters as properties?????  if so then I need to strigify the data??????)
+        // need a function that creates a button with the key of the stored city data displayed
+            // might need to use for loop to create the buttons and render the names of the cities in previously stored object array ???????
+    
+    
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city  
+            // need click event listener (similar to one in daily planner that runs no matter what button is clicked) the executes code when button from search history is clicked
+        // need a function that pulls the data stored in the local storage back to thier appropriate divs
+
 
