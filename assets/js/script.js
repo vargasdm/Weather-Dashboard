80c6accce485bb6960b6ebcc9d4bcf8db88d14fb
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-11-20 08:39:49 -0500

    added an window.onload line to render search history buttons when the page loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,6 @@ window.onload = renderSearchHistory();
         // need a function that will use the textarea input as parameters ( somhow convert the name into coordinates that can be used by the api ) for the fetch function
         $("#search").click(function (event) {
             cityName = $(this).siblings('textarea').val();  
-            console.log(cityName);
             var queryURL = baseUrl + cityName + "&appid=" + APIKey + "&units=imperial";
         //    fetching data for the daily forecast
             fetch(queryURL, {})
@@ -31,10 +30,7 @@ window.onload = renderSearchHistory();
                 })
                 .then(function (data) {
                 console.log(data);
-                console.log(data.weather[0].description);
                 weatherData = data;
-                console.log(weatherData);
-                console.log(weatherData.name)
             
             // fetching data to get eh lat and lon values for the searched city to be used in teh 5 day forecast fetch request
                 var queryGeoUrl = geocodeBaseUrl + cityName + "&appid=" + APIKey
@@ -45,11 +41,8 @@ window.onload = renderSearchHistory();
                     .then(function (data) {
                     console.log(data);
                 var geoData = data
-                console.log(geoData);
                 var geoLat = geoData[0].lat
-                console.log(geoLat);
                 var geoLon = geoData[0].lon
-                console.log(geoLon);
 
                 // fetching data for the 5 day forcast
                 var queryFiveDayUrl = fiveDayBaseUrl + "lat=" + geoLat + "&lon=" + geoLon + "&appid=" + APIKey + "&units=imperial";
@@ -73,20 +66,15 @@ window.onload = renderSearchHistory();
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
                 function displayCurrentWeather() {
                     currentWeatherEl.innerHTML = "";
-                    console.log(currentWeatherName)
                     var currentWeatherName = document.createElement('h2');
                     currentWeatherName.textContent = weatherData.name;
-                    console.log(currentWeatherName)
                     currentWeatherEl.appendChild(currentWeatherName);
 
-                    console.log(weatherData.dt)
                     var date = dayjs.unix(weatherData.dt) //.format('MMM D, YYYY, hh:mm:ss a');
-                    console.log(date);
                     var currentWeatherDate = document.createElement('p');
                     currentWeatherDate.textContent = date;
                     currentWeatherEl.appendChild(currentWeatherDate);
                     
-                    console.log(weatherData.weather[0].icon)
                     var currentWeatherIcon = document.createElement('img');
                     var iconUrl = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
                     currentWeatherIcon.setAttribute('src', iconUrl);
@@ -94,17 +82,14 @@ window.onload = renderSearchHistory();
 
                     var currentWeatherTemp = document.createElement('p');
                     currentWeatherTemp.textContent = "Temp: " + weatherData.main.temp + "°F";
-                    console.log(currentWeatherTemp)
                     currentWeatherEl.appendChild(currentWeatherTemp);
 
                     var currentWeatherHum = document.createElement('p');
                     currentWeatherHum.textContent = "Humidity: " + weatherData.main.humidity + "%";
-                    console.log(currentWeatherHum)
                     currentWeatherEl.appendChild(currentWeatherHum);
 
                     var currentWeatherWind = document.createElement('p');
                     currentWeatherWind.textContent = "Wind: " + weatherData.wind.speed + " MPH";
-                    console.log(currentWeatherWind)
                     currentWeatherEl.appendChild(currentWeatherWind); 
                 }
 
@@ -151,8 +136,6 @@ window.onload = renderSearchHistory();
    // local storage
    function appendLocalStorage() {
         if (cityName !== "") {
-            console.log(searchArr);
-
             if (searchArr.indexOf(cityName) !== -1) {
                 return;
              }
@@ -164,9 +147,10 @@ window.onload = renderSearchHistory();
    }
 
     function renderSearchHistory() {
+        searchHistory.innerHTML = "";
         searchHistoryList = searchArr;
         console.log(searchHistoryList);
-        searchHistory.innerHTML = "";
+        
         for (var i = searchHistoryList.length - 1; i >= 0; i--) {
             var searchHistoryItem = document.createElement('button');
             searchHistoryItem.textContent = searchHistoryList[i];
